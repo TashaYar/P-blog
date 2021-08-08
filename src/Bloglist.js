5993bf852df91499707558aefb18f597141d9ff9
Pallavi Singh <pgs909294@gmail.com>
2021-08-08 11:28:28 +0530

    lo

---
+++ src/Bloglist.js
@@ -0,0 +1,16 @@
+const Bloglist = ({ blogs, title, handleDelete }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+      {blogs.map((blogs) => (
+        <div className="blog-preview" key={blogs.id}>
+          <h2>{blogs.title}</h2>
+          <p>Written by {blogs.author}</p>
+          <button onClick={() => handleDelete(blogs.id)}>Delete blog</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Bloglist;
