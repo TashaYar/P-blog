a156c86eb37195b759672b8efb74981f88dd6a6a
Pallavi Singh <pgs909294@gmail.com>
2021-08-13 12:13:17 +0530

    hand

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,4 +1,4 @@
-const Bloglist = ({ blogs, title, handleDelete }) => {
+const Bloglist = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -6,7 +6,6 @@ const Bloglist = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blogs.id}>
           <h2>{blogs.title}</h2>
           <p>Written by {blogs.author}</p>
-          <button onClick={() => handleDelete(blogs.id)}>Delete blog</button>
         </div>
       ))}
     </div>
